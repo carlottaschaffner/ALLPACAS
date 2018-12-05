eaf34d187cbf9479708605f7ee31be6b1916d9bd
Daniel Moreira <de8a88@gmail.com>
2018-12-05 13:31:25 +0000

    removed authentication from show page

--- app/controllers/alpacas_controller.rb
+++ app/controllers/alpacas_controller.rb
@@ -1,5 +1,5 @@
 class AlpacasController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @alpacas = policy_scope(Alpaca).order(created_at: :desc)
