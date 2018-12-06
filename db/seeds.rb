8d904b9107597fd8376a22f1540d85a5651b48be
Daniel Moreira <de8a88@gmail.com>
2018-12-06 16:02:39 +0000

    fixed broken link when there were no reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Alpaca.destroy_all
-User.destroy_all
 
 puts 'Creating users...'
 names = ["Carlotta", "Daniel", "Ines"]
