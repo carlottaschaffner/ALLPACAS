746f65c49ca7eedc604a1a1768cf5ae7bcbf50f5
Jonny Marshall <jonnymarshall5@gmail.com>
2018-12-06 17:36:45 +0000

    added proper email seeding and firstname, lastname to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_150452) do
+ActiveRecord::Schema.define(version: 2018_12_06_173002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 2018_12_06_150452) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
-    t.text "description"
     t.string "photo"
+    t.text "description"
     t.index ["user_id"], name: "index_alpacas_on_user_id"
   end
 
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 2018_12_06_150452) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
